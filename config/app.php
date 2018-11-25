70f466b46bfe2a16ebf583ce97fc5d3ee5928def
grandeto <grandeto.com@gmail.com>
2018-11-25 16:09:40 +0200

    Change app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Shippng system'),
+    'name' => env('APP_NAME', 'Shipping system'),
 
     /*
     |--------------------------------------------------------------------------
