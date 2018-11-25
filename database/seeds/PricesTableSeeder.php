f5702e0e87a0c95e58d786c347c1321b49c84c28
grandeto <grandeto.com@gmail.com>
2018-11-25 16:10:13 +0200

    Add more price seeds

--- database/seeds/PricesTableSeeder.php
+++ database/seeds/PricesTableSeeder.php
@@ -22,6 +22,9 @@ class PricesTableSeeder extends Seeder
             array('method_id' => 6, 'origin_country_id' => 1, 'destination_country_id' => 58, 'min_weight' => 10, 'max_weight' => 20, 'price' => 557.99),
             array('method_id' => 7, 'origin_country_id' => 1, 'destination_country_id' => 58, 'min_weight' => 10, 'max_weight' => 20, 'price' => 108.67),
             array('method_id' => 8, 'origin_country_id' => 1, 'destination_country_id' => 58, 'min_weight' => 10, 'max_weight' => 20, 'price' => 530),
+
+            array('method_id' => 3, 'origin_country_id' => 58, 'destination_country_id' => 35, 'min_weight' => 5, 'max_weight' => 10, 'price' => 50),
+            array('method_id' => 4, 'origin_country_id' => 58, 'destination_country_id' => 35, 'min_weight' => 5, 'max_weight' => 10, 'price' => 117.07),
         );
 
         DB::table('prices')->insert($prices);
